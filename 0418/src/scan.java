da3636f154b06471bdf49b79d7aab9787f06857b
Administrator <Administrator@501-S19>
2022-04-18 10:03:15 +0900

    0418 프로젝트 추가

--- 0418/src/scan.java
+++ 0418/src/scan.java
@@ -4,12 +4,19 @@ public class scan {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);   
-		int sum=0;  int a,b=0;
 		System.out.println("1000 이하의 자연수를 입력하세요.");
-		   a =sc.nextInt();
-		   for(int i =a; i<=b;i++) {
+		int num1 = sc.nextInt();
+		int sum = 0;
+		int i =1;
+	 	while (i<=num1) {
+			if(i%4==0) {
+				System.out.println(i);
+				sum +=i;
+			}
+			i++;
+		}
+	 	System.out.println("1~"+num1+"까지 4의 배수의 합은"+sum+"입니다.");
 			
-		   }
 	}
 
 }
