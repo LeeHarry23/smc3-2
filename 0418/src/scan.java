b6cd0d296aa30d1a8fcda72efe53575c76e8b3b5
Administrator <Administrator@501-S19>
2022-04-18 09:55:11 +0900

    0418 프로젝트 추가

--- 0418/src/scan.java
+++ 0418/src/scan.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class scan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);   
+		int sum=0;  int a,b=0;
+		System.out.println("1000 이하의 자연수를 입력하세요.");
+		   a =sc.nextInt();
+		   for(int i =a; i<=b;i++) {
+			
+		   }
 	}
 
 }
